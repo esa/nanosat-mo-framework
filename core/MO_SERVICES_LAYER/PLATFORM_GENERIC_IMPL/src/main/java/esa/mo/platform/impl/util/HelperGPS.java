7e157c5c73a2660f0c1957502ef2e7068bc49668
Cezar Suteu <Cezar.Suteu@esa.int>
2016-06-14 17:16:20 +0200

    [core] Added gps implementations
    Former-commit-id: 71c62fc86aa3ff29234ff2c45e7fdc4c4c5071f5

--- core/MO_SERVICES_LAYER/PLATFORM_GENERIC_IMPL/src/main/java/esa/mo/platform/impl/util/HelperGPS.java
+++ core/MO_SERVICES_LAYER/PLATFORM_GENERIC_IMPL/src/main/java/esa/mo/platform/impl/util/HelperGPS.java
@@ -23,73 +23,12 @@ package esa.mo.platform.impl.util;
 import org.ccsds.moims.mo.platform.gps.structures.Position;
 import org.ccsds.moims.mo.platform.gps.structures.PositionExtraDetails;
 
-
 /**
  *
  * @author Cesar Coelho
  */
 public class HelperGPS {
 
-    public final static int GPGGA_HEADER_COL            =0;
-    public final static int GPGGA_UTC_COL               =1;
-    public final static int GPGGA_LAT_COL               =2;
-    public final static int GPGGA_LAT_DIR_COL           =3;
-    public final static int GPGGA_LONG_COL              =4;
-    public final static int GPGGA_LONG_DIR_COL          =5;
-    public final static int GPGGA_QUAL_COL              =6;
-    public final static int GPGGA_SATS_IN_USE_COL       =7;
-    public final static int GPGGA_HDOP_COL              =8;
-    public final static int GPGGA_ALTITUDE_COL          =9;
-    public final static int GPGGA_ALTITUDE_UNITS_COL    =10;
-    public final static int GPGGA_UNDULATION_COL        =11;
-    public final static int GPGGA_UNDULATION_UNITS_COL  =12;
-    public final static int GPGGA_AGE_CORR_DATA_COL     =13;
-    public final static int GPGGA_DIFF_BASESID_COL      =14;
-    public final static int GPGGA_CHECKSUM_COL          =15;
     
-    public static double DDMMpMMMM2degrees(String DDMMpMMMM)
-    { 
-        if (DDMMpMMMM.length()==9)
-        {
-            //System.out.println("DDMMpMMMM["+DDMMpMMMM+"]");
-            //System.out.println(Double.parseDouble(DDMMpMMMM.substring(5, 9))/3600);
-            return Double.parseDouble(DDMMpMMMM.substring(0, 2))+(Double.parseDouble(DDMMpMMMM.substring(2, 4))+Double.parseDouble(DDMMpMMMM.substring(5, 9))/1000)/60;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    /**
-     * Converts a GPGGA NMEA sentence into a partial Position object.
-     *
-     * @param gpgga GPGGA NMEA sentence
-     * @return Position object
-     */
-    public static Position gpgga2partialPosition(String gpgga){
-        Position pos = new Position();
-        String [] items = gpgga.split(",");
-        pos.setAltitude(Double.parseDouble(items[GPGGA_ALTITUDE_COL]));
-        pos.setLatitude(DDMMpMMMM2degrees(items[GPGGA_LAT_COL])*((items[GPGGA_LAT_DIR_COL]).equals("S")?-1:1));
-        pos.setLongitude(DDMMpMMMM2degrees(items[GPGGA_LONG_COL])*((items[GPGGA_LONG_DIR_COL]).equals("W")?-1:1));
-        return pos;
-    }
 
-    /**
-     * Converts a GPGGA NMEA sentence and SMTH into a full Position object.
-     *
-     * @param gpgga GPGGA NMEA sentence
-     * @param smth
-     * @return Position object
-     */
-    public static Position gpgga2fullPosition(String gpgga, String smth){
-
-        Position pos = gpgga2partialPosition(gpgga);
-        PositionExtraDetails posExtraDetails=new PositionExtraDetails();
-        pos.setExtraDetails(posExtraDetails);
-        return pos;
-    }
-    
-    
-       
 }
