e63e5b399fa8b0dc9dc04a0bb3f324d8de792998
Cezar Suteu <Cezar.Suteu@esa.int>
2016-08-18 19:40:24 +0200

    [core] Modified simulator device datas, added more parameters to fineADCS, added scheduler startup values assignments, added some helper ADCS implementation
    Former-commit-id: 9b183c7b4be3d5d5c2bc31a51c898c71041d30f6

--- core/MO_SERVICES_LAYER/PLATFORM_GENERIC_IMPL/src/main/java/esa/mo/platform/impl/util/HelperADCS.java
+++ core/MO_SERVICES_LAYER/PLATFORM_GENERIC_IMPL/src/main/java/esa/mo/platform/impl/util/HelperADCS.java
@@ -20,6 +20,8 @@
  */
 package esa.mo.platform.impl.util;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import org.ccsds.moims.mo.platform.structures.Quaternions;
 import org.ccsds.moims.mo.platform.structures.Vector3D;
 import org.ccsds.moims.mo.platform.structures.WheelSpeed;
@@ -30,9 +32,115 @@ import org.ccsds.moims.mo.platform.structures.WheelSpeed;
  * @author Cesar Coelho
  */
 public class HelperADCS {
-
+    public static class FWRefFineADCS {
+        public static class SENSORTM_IDX{
+            //Byte offset
+            public final static int MAG_FIELD_X                      = 0*4;
+            public final static int MAG_FIELD_Y                      = 1*4;
+            public final static int MAG_FIELD_Z                      = 2*4;
+            public final static int ACCELEROMETER_X                  = 3*4;
+            public final static int ACCELEROMETER_Y                  = 4*4;
+            public final static int ACCELEROMETER_Z                  = 5*4;
+            public final static int GYRO1_X                          = 6*4;
+            public final static int GYRO1_Y                          = 7*4;
+            public final static int GYRO1_Z                          = 8*4;
+            public final static int GYRO2_X                          = 9*4;
+            public final static int GYRO2_Y                          = 10*4;
+            public final static int GYRO2_Z                          = 11*4;
+            public final static int ST200_TIME                       = 12*4;//UI64
+            public final static int ST200_TIME_MSEC                  = 14*4;//UI16
+            public final static int QUATERNION1                      = 14*4+2;
+            public final static int QUATERNION2                      = 15*4+2;
+            public final static int QUATERNION3                      = 16*4+2;
+            public final static int QUATERNION4                      = 17*4+2;
+        }
+        public static byte [] long2ByteArray (long value)
+        {
+            return ByteBuffer.allocate(Long.BYTES).putLong(value).array();
+        }
+        public static byte [] int2ByteArray (int value)
+        {  
+             return ByteBuffer.allocate(Integer.BYTES).putInt(value).array();
+        }
+        public static byte [] int16_2ByteArray (int value)
+        {  
+             byte[] temp=ByteBuffer.allocate(Integer.BYTES).putInt(value).array();
+             byte[] result=new byte[2];
+             result[0]=temp[2];
+             result[1]=temp[3];
+             return result;
+        }
+        public static byte [] float2ByteArray (float value)
+        {  
+             return ByteBuffer.allocate(Float.BYTES).putFloat(value).array();
+        }
+        public static byte [] double2ByteArray (double value)
+        {  
+             return ByteBuffer.allocate(Double.BYTES).putDouble(value).array();
+        }
+        
+        public static void putFloatInByteArray(float value, int byteOffset, byte[] target)
+        {
+            byte [] tempByte=float2ByteArray(value);
+            for (int i=0;i<=Float.BYTES-1;i++)
+            {
+                target[byteOffset+i]=tempByte[i];
+            }
+        }
+        public static float getFloatFromByteArray(byte[] source, int byteOffset)
+        {
+            return ByteBuffer.wrap(source,byteOffset,Float.BYTES).order(ByteOrder.BIG_ENDIAN).getFloat();
+        }
+        public static void putDoubleInByteArray(double value, int byteOffset, byte[] target)
+        {
+            byte [] tempByte=double2ByteArray(value);
+            for (int i=0;i<=Double.BYTES-1;i++)
+            {
+                target[byteOffset+i]=tempByte[i];
+            }
+        }
+        public static double getDoubleFromByteArray(byte[] source, int byteOffset)
+        {
+            return ByteBuffer.wrap(source,byteOffset,Double.BYTES).order(ByteOrder.BIG_ENDIAN).getDouble();
+        }
+        public static void putIntInByteArray(int value, int byteOffset, byte[] target)
+        {
+            byte [] tempByte=int2ByteArray(value);
+            for (int i=0;i<=Integer.BYTES-1;i++)
+            {
+                target[byteOffset+i]=tempByte[i];
+            }
+        }
+        public static int getIntFromByteArray(byte[] source, int byteOffset)
+        {
+            return ByteBuffer.wrap(source,byteOffset,Integer.BYTES).order(ByteOrder.BIG_ENDIAN).getInt();
+        }
+        public static int getInt16FromByteArray(byte[] source, int byteOffset)
+        {
+            byte[] temp=new byte[Integer.BYTES];
+            temp[2]=source[byteOffset];
+            temp[3]=source[byteOffset+1];
+            return ByteBuffer.wrap(temp,byteOffset,Integer.BYTES).order(ByteOrder.BIG_ENDIAN).getInt();
+        }
+        public static void putLongInByteArray(long value, int byteOffset, byte[] target)
+        {
+            byte [] tempByte=long2ByteArray(value);
+            for (int i=0;i<=Long.BYTES-1;i++)
+            {
+                target[byteOffset+i]=tempByte[i];
+            }
+        }
+        public static long getLongFromByteArray(byte[] source, int byteOffset)
+        {
+            return ByteBuffer.wrap(source,byteOffset,Long.BYTES).order(ByteOrder.BIG_ENDIAN).getLong();
+        }
+    
+    }
     public static Vector3D getMagneticFieldFromSensorTM(byte[] sensorTM) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new Vector3D(
+            FWRefFineADCS.getDoubleFromByteArray(sensorTM, FWRefFineADCS.SENSORTM_IDX.MAG_FIELD_X),
+            FWRefFineADCS.getDoubleFromByteArray(sensorTM, FWRefFineADCS.SENSORTM_IDX.MAG_FIELD_Y),
+            FWRefFineADCS.getDoubleFromByteArray(sensorTM, FWRefFineADCS.SENSORTM_IDX.MAG_FIELD_Z));        
     }
 
     public static Vector3D getMTQFromActuatorTM(byte[] actuatorTM) {
