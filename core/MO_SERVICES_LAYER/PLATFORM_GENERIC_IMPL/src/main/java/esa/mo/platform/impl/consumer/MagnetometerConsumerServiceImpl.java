aa5c3f610f4875d1db2bd553407f5dce5b14203d
Cesar Coelho <cesar.coelho@esa.int>
2016-08-22 14:10:21 +0200

    [core] Added the missing consumer service stubs implementations in the Generic Platform services implementation.
    Former-commit-id: 4ed88107876584491ae32bac25a920dda92aa8df

---
+++ core/MO_SERVICES_LAYER/PLATFORM_GENERIC_IMPL/src/main/java/esa/mo/platform/impl/consumer/MagnetometerConsumerServiceImpl.java
@@ -0,0 +1,85 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.platform.impl.consumer;
+
+import esa.mo.com.impl.util.COMServicesConsumer;
+import esa.mo.helpertools.misc.ConsumerServiceImpl;
+import esa.mo.helpertools.connections.SingleConnectionDetails;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.ccsds.moims.mo.mal.MALException;
+import org.ccsds.moims.mo.mal.MALInteractionException;
+import org.ccsds.moims.mo.mal.consumer.MALConsumer;
+import org.ccsds.moims.mo.platform.magnetometer.MagnetometerHelper;
+import org.ccsds.moims.mo.platform.magnetometer.consumer.MagnetometerStub;
+
+/**
+ *
+ * @author Cesar Coelho
+ */
+public class MagnetometerConsumerServiceImpl extends ConsumerServiceImpl {
+
+    private MagnetometerStub magnetometerService = null;
+    private COMServicesConsumer comServices;
+
+    public COMServicesConsumer getCOMServices() {
+        return comServices;
+    }
+
+    @Override
+    public Object getStub() {
+        return this.getGPSStub();
+    }
+
+    public MagnetometerStub getGPSStub() {
+        return this.magnetometerService;
+    }
+
+    @Override
+    public Object generateServiceStub(MALConsumer tmConsumer) {
+        return new MagnetometerStub(tmConsumer);
+    }
+
+    public MagnetometerConsumerServiceImpl(SingleConnectionDetails connectionDetails, COMServicesConsumer comServices) throws MALException, MalformedURLException, MALInteractionException {
+
+        this.connectionDetails = connectionDetails;
+        this.comServices = comServices;
+
+        // Close old connection
+        if (tmConsumer != null) {
+            try {
+                tmConsumer.close();
+            } catch (MALException ex) {
+                Logger.getLogger(MagnetometerConsumerServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        tmConsumer = connection.startService(
+                this.connectionDetails.getProviderURI(),
+                this.connectionDetails.getBrokerURI(),
+                this.connectionDetails.getDomain(),
+                MagnetometerHelper.MAGNETOMETER_SERVICE);
+
+        this.magnetometerService = new MagnetometerStub(tmConsumer);
+    }
+
+}
---
+++ core/MO_SERVICES_LAYER/PLATFORM_GENERIC_IMPL/src/main/java/esa/mo/platform/impl/consumer/PowerControlServiceImpl.java
@@ -0,0 +1,85 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.platform.impl.consumer;
+
+import esa.mo.com.impl.util.COMServicesConsumer;
+import esa.mo.helpertools.misc.ConsumerServiceImpl;
+import esa.mo.helpertools.connections.SingleConnectionDetails;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.ccsds.moims.mo.mal.MALException;
+import org.ccsds.moims.mo.mal.MALInteractionException;
+import org.ccsds.moims.mo.mal.consumer.MALConsumer;
+import org.ccsds.moims.mo.platform.powercontrol.PowerControlHelper;
+import org.ccsds.moims.mo.platform.powercontrol.consumer.PowerControlStub;
+
+/**
+ *
+ * @author Cesar Coelho
+ */
+public class PowerControlServiceImpl extends ConsumerServiceImpl {
+
+    private PowerControlStub powerControlService = null;
+    private COMServicesConsumer comServices;
+
+    public COMServicesConsumer getCOMServices() {
+        return comServices;
+    }
+
+    @Override
+    public Object getStub() {
+        return this.getGPSStub();
+    }
+
+    public PowerControlStub getGPSStub() {
+        return this.powerControlService;
+    }
+
+    @Override
+    public Object generateServiceStub(MALConsumer tmConsumer) {
+        return new PowerControlStub(tmConsumer);
+    }
+
+    public PowerControlServiceImpl(SingleConnectionDetails connectionDetails, COMServicesConsumer comServices) throws MALException, MalformedURLException, MALInteractionException {
+
+        this.connectionDetails = connectionDetails;
+        this.comServices = comServices;
+
+        // Close old connection
+        if (tmConsumer != null) {
+            try {
+                tmConsumer.close();
+            } catch (MALException ex) {
+                Logger.getLogger(PowerControlServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        tmConsumer = connection.startService(
+                this.connectionDetails.getProviderURI(),
+                this.connectionDetails.getBrokerURI(),
+                this.connectionDetails.getDomain(),
+                PowerControlHelper.POWERCONTROL_SERVICE);
+
+        this.powerControlService = new PowerControlStub(tmConsumer);
+    }
+
+}
