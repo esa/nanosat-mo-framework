014a61490dacfb91036bbc6ac62a677fb27fa691
Cesar Coelho <cesar.coelho@esa.int>
2016-08-22 10:16:41 +0200

    [core] AutonomousADCS service consumer. Generation of the NanoSat MO framework library for the Software Simulator
    Former-commit-id: c8ac777f595fac71ebc3bfd4da45038c3c7018c8

---
+++ core/MO_SERVICES_LAYER/PLATFORM_GENERIC_IMPL/src/main/java/esa/mo/platform/impl/consumer/AutonomousADCSConsumerServiceImpl.java
@@ -0,0 +1,85 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.platform.impl.consumer;
+
+import esa.mo.com.impl.util.COMServicesConsumer;
+import esa.mo.helpertools.misc.ConsumerServiceImpl;
+import esa.mo.helpertools.connections.SingleConnectionDetails;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.ccsds.moims.mo.mal.MALException;
+import org.ccsds.moims.mo.mal.MALInteractionException;
+import org.ccsds.moims.mo.mal.consumer.MALConsumer;
+import org.ccsds.moims.mo.platform.autonomousadcs.AutonomousADCSHelper;
+import org.ccsds.moims.mo.platform.autonomousadcs.consumer.AutonomousADCSStub;
+
+/**
+ *
+ * @author Cesar Coelho
+ */
+public class AutonomousADCSConsumerServiceImpl extends ConsumerServiceImpl {
+
+    private AutonomousADCSStub autonomousADCSService = null;
+    private COMServicesConsumer comServices;
+
+    public COMServicesConsumer getCOMServices() {
+        return comServices;
+    }
+
+    @Override
+    public Object getStub() {
+        return this.getCameraStub();
+    }
+
+    public AutonomousADCSStub getCameraStub() {
+        return this.autonomousADCSService;
+    }
+
+    @Override
+    public Object generateServiceStub(MALConsumer tmConsumer) {
+        return new AutonomousADCSStub(tmConsumer);
+    }
+
+    public AutonomousADCSConsumerServiceImpl(SingleConnectionDetails connectionDetails, COMServicesConsumer comServices) throws MALException, MalformedURLException, MALInteractionException {
+
+        this.connectionDetails = connectionDetails;
+        this.comServices = comServices;
+
+        // Close old connection
+        if (tmConsumer != null) {
+            try {
+                tmConsumer.close();
+            } catch (MALException ex) {
+                Logger.getLogger(AutonomousADCSConsumerServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        tmConsumer = connection.startService(
+                this.connectionDetails.getProviderURI(),
+                this.connectionDetails.getBrokerURI(),
+                this.connectionDetails.getDomain(),
+                AutonomousADCSHelper.AUTONOMOUSADCS_SERVICE);
+
+        this.autonomousADCSService = new AutonomousADCSStub(tmConsumer);
+    }
+
+}
