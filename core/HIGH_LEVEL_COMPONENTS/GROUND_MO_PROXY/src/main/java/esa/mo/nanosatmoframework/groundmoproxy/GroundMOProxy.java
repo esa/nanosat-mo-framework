ab52c0e0b45e88844ea3636443a25b82ecd1d30c
Cesar Coelho <cesar.coelho@esa.int>
2016-12-15 16:48:55 +0100

    [core] Feature: Getting things ready for Ground MO Proxy.
    Former-commit-id: 9324b9beb375d07280494065fb8c4e9aaeeae322

--- core/HIGH_LEVEL_COMPONENTS/GROUND_MO_PROXY/src/main/java/esa/mo/nanosatmoframework/groundmoproxy/GroundMOProxy.java
+++ core/HIGH_LEVEL_COMPONENTS/GROUND_MO_PROXY/src/main/java/esa/mo/nanosatmoframework/groundmoproxy/GroundMOProxy.java
@@ -24,35 +24,33 @@ import esa.mo.com.impl.provider.ArchiveProviderServiceImpl;
 import esa.mo.com.impl.util.COMServicesProvider;
 import esa.mo.helpertools.connections.ConnectionConsumer;
 import esa.mo.nanosatmoframework.groundmoadapter.GroundMOAdapter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.ccsds.moims.mo.common.directory.structures.ProviderSummary;
 import org.ccsds.moims.mo.mal.MALException;
 
-
 /**
  * A Consumer of MO services composed by COM, M&C and Platform services.
  * Implements the SimpleCommandingInterface that permits an external software
- * entity to send data (parameters or serialized objects) to the provider, add
- * a DataReceivedListener to receive data and send actions to the provider.
- * It extends the MOServicesConsumer class in order make available the pure MO 
+ * entity to send data (parameters or serialized objects) to the provider, add a
+ * DataReceivedListener to receive data and send actions to the provider. It
+ * extends the MOServicesConsumer class in order make available the pure MO
  * interfaces.
- * 
+ *
  * @author Cesar Coelho
  */
-public class GroundMOProxy  {
+public class GroundMOProxy {
+
+    private final COMServicesProvider comServicesProxy = new COMServicesProvider();
 
     // Have a list of providers
-    private COMServicesProvider comServicesProxy = new COMServicesProvider();
-    private HashMap<String, GroundMOAdapter> myProviders = new HashMap<String, GroundMOAdapter>();
-    
-    /**
-     * The constructor of this class
-     *
-     * @param connection The connection details of the provider
-     */
-    public GroundMOProxy(ConnectionConsumer connection) {
+    private final HashMap<String, GroundMOAdapter> myProviders = new HashMap<String, GroundMOAdapter>();
+
+    public void init() {
+
+        // Initialize the extension bridge services and expose them using TCP/IP !
 
         try {
             comServicesProxy.init();
@@ -61,22 +59,49 @@ public class GroundMOProxy  {
         }
         
         
-
-    }
-
-    /**
-     * The constructor of this class
-     *
-     * @param providerDetails The Provider details. This object can be obtained 
-     * from the Directory service
-     */
-    public GroundMOProxy(ProviderSummary providerDetails) {
-
-    }
-    
-    public void connectToProvider(ProviderSummary providerDetails){
+        // Initialize the pure protocol bridge for the services without extension
+        // HERE
         
     }
 
+    /**
+     *
+     * @param connection The connection details of the provider
+     * @return
+     * @throws java.io.IOException
+     */
+    public GroundMOAdapter connectToProvider(ConnectionConsumer connection) throws IOException {
+
+        synchronized(myProviders){
+            final String key = "vvfjvfbjsdkvfbsksdfksdfkbvf"; // To be done
+            GroundMOAdapter gma = myProviders.get(key);
+            
+            if(gma != null){
+                throw new IOException("The proxy is already connected to this Provider!");
+            }
+            
+            gma = new GroundMOAdapter(connection);
+            
+            myProviders.put(key, gma);
+            
+            return gma;
+        }
+        
+        
+    }
+
+    /**
+     *
+     * @param providerDetails The Provider details. This object can be obtained
+     * from the Directory service
+     * @return
+     * @throws java.io.IOException
+     */
+    public GroundMOAdapter connectToProvider(ProviderSummary providerDetails) throws IOException {
+        
+        // To be done...
+        
+        return null;
+    }
 
 }
