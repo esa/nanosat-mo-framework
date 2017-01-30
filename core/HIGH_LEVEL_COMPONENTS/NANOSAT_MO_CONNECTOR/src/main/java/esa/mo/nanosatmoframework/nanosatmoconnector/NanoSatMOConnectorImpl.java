dfe754a8c9ed721fb6cd61f8ef5487fb0b3612f7
Cesar Coelho <cesar.coelho@esa.int>
2017-01-30 19:02:35 +0100

    [core] Polishing: NMC class
    Former-commit-id: beb19fef5d1dff72e9f2b5045b26296ed5102709

--- core/HIGH_LEVEL_COMPONENTS/NANOSAT_MO_CONNECTOR/src/main/java/esa/mo/nanosatmoframework/nanosatmoconnector/NanoSatMOConnectorImpl.java
+++ core/HIGH_LEVEL_COMPONENTS/NANOSAT_MO_CONNECTOR/src/main/java/esa/mo/nanosatmoframework/nanosatmoconnector/NanoSatMOConnectorImpl.java
@@ -114,9 +114,13 @@ public final class NanoSatMOConnectorImpl extends NanoSatMOFrameworkProvider {
                 IdentifierList domain = new IdentifierList();
                 domain.add(new Identifier("*"));
                 COMService eventCOM = EventHelper.EVENT_SERVICE; // Filter for the Event service of the Supervisor
-                ServiceKey serviceKey = new ServiceKey(eventCOM.getArea().getNumber(), eventCOM.getNumber(), eventCOM.getArea().getVersion());
-                ServiceFilter sf = new ServiceFilter(new Identifier(NanoSatMOFrameworkProvider.NANOSAT_MO_SUPERVISOR_NAME), domain, new Identifier("*"), null, new Identifier("*"), serviceKey, new UIntegerList());
-                ProviderSummaryList supervisorEventServiceConnectionDetails = directoryServiceConsumer.getDirectoryStub().lookupProvider(sf);
+                final ServiceKey serviceKey = new ServiceKey(eventCOM.getArea().getNumber(), 
+                        eventCOM.getNumber(), eventCOM.getArea().getVersion());
+                final ServiceFilter sf = new ServiceFilter(
+                        new Identifier(NanoSatMOFrameworkProvider.NANOSAT_MO_SUPERVISOR_NAME), 
+                        domain, new Identifier("*"), null, new Identifier("*"), 
+                        serviceKey, new UIntegerList());
+                final ProviderSummaryList supervisorEventServiceConnectionDetails = directoryServiceConsumer.getDirectoryStub().lookupProvider(sf);
 
                 // Register for CloseApp Events...
                 try {
@@ -131,32 +135,39 @@ public final class NanoSatMOConnectorImpl extends NanoSatMOFrameworkProvider {
                 // Select all object numbers from the Apps Launcher service Events
                 final Long secondEntityKey = 0xFFFFFFFFFF000000L & HelperCOM.generateSubKey(AppsLauncherHelper.APP_OBJECT_TYPE);
                 final Random random = new Random();
-                subscription = ConnectionConsumer.subscriptionKeys(new Identifier("CloseAppEventListener" + random.nextInt()), new Identifier("*"), secondEntityKey, new Long(0), new Long(0));
+                subscription = ConnectionConsumer.subscriptionKeys(new Identifier("CloseAppEventListener" + random.nextInt()), 
+                        new Identifier("*"), secondEntityKey, new Long(0), new Long(0));
 
                 // Register with the subscription key provided
                 serviceCOMEvent.addEventReceivedListener(subscription, new CloseAppEventListener(this));
 
                 // Lookup for the Platform services on the NanoSat MO Supervisor
-                ServiceKey sk = new ServiceKey(PlatformHelper.PLATFORM_AREA_NUMBER, new UShort(0), new UOctet((short) 0));
-                ServiceFilter sf2 = new ServiceFilter(new Identifier(NanoSatMOFrameworkProvider.NANOSAT_MO_SUPERVISOR_NAME), domain, new Identifier("*"), null, new Identifier("*"), sk, new UIntegerList());
-                ProviderSummaryList supervisorConnections = directoryServiceConsumer.getDirectoryStub().lookupProvider(sf2);
+                final ServiceKey sk = new ServiceKey(PlatformHelper.PLATFORM_AREA_NUMBER, 
+                        new UShort(0), new UOctet((short) 0));
+                final ServiceFilter sf2 = new ServiceFilter(
+                        new Identifier(NanoSatMOFrameworkProvider.NANOSAT_MO_SUPERVISOR_NAME), 
+                        domain, new Identifier("*"), null, new Identifier("*"), sk, new UIntegerList());
+                final ProviderSummaryList supervisorConnections = directoryServiceConsumer.getDirectoryStub().lookupProvider(sf2);
 
                 if (supervisorConnections.size() == 1) { // Platform services found!
-                    // Load the all the Platform services' APIs
+                    // Load all the Platform services' APIs
                     if (MALContextFactory.lookupArea(PlatformHelper.PLATFORM_AREA_NAME, PlatformHelper.PLATFORM_AREA_VERSION) == null) {
                         PlatformHelper.deepInit(MALContextFactory.getElementFactoryRegistry());
                     }
 
-                    ConnectionConsumer supervisorCD = HelperCommon.providerSummaryToConnectionConsumer(supervisorConnections.get(0));
+                    ConnectionConsumer supervisorCCPlat = HelperCommon.providerSummaryToConnectionConsumer(supervisorConnections.get(0));
 
                     // Connect to them...
                     platformServices = new PlatformServicesConsumer();
                     COMServicesConsumer comServicesConsumer = new COMServicesConsumer();
-                    comServicesConsumer.init(supervisorCD);
-                    platformServices.init(supervisorCD, comServicesConsumer);
-                    Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.INFO, "Successfully connected to: " + supervisorCD.getConsumerURI());
+                    comServicesConsumer.init(supervisorCCPlat);
+                    platformServices.init(supervisorCCPlat, comServicesConsumer);
+                    Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.INFO,
+                            "Successfully connected to: " + supervisorCCPlat.getConsumerURI());
                 } else {
-                    Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.SEVERE, "The Connector was expecting a single NanoSat MO Supervisor provider! Instead it found " + supervisorConnections.size() + ".");
+                    Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.SEVERE,
+                            "The NanoSat MO Connector was expecting a single NanoSat MO Supervisor provider! Instead it found "
+                            + supervisorConnections.size() + ".");
                 }
             } catch (MALException ex) {
                 Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -179,7 +190,7 @@ public final class NanoSatMOConnectorImpl extends NanoSatMOFrameworkProvider {
                     "The services could not be initialized. Perhaps there's something wrong with the Transport Layer.", ex);
             return;
         }
-        
+
         // Populate the Directory service with the entries from the URIs File
         Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.INFO, "Populating Local Directory service...");
         PublishDetails publishDetails = directoryService.autoLoadURIsFile(this.providerName);
@@ -195,11 +206,11 @@ public final class NanoSatMOConnectorImpl extends NanoSatMOFrameworkProvider {
                 }
             }
 
-            MCRegistration registration = new MCRegistration(comServices, mcServices.getParameterService(), 
+            MCRegistration registration = new MCRegistration(comServices, mcServices.getParameterService(),
                     mcServices.getAggregationService(), mcServices.getAlertService(), mcServices.getActionService());
             mcAdapter.initialRegistrations(registration);
         }
-        
+
         if (centralDirectoryURI != null) {
             try {
                 // Register the services in the Central Directory service...
@@ -228,9 +239,9 @@ public final class NanoSatMOConnectorImpl extends NanoSatMOFrameworkProvider {
     }
 
     public final URI readCentralDirectoryServiceURI() {
-        String path = ".." 
+        String path = ".."
                 + File.separator
-                + NanoSatMOFrameworkProvider.NANOSAT_MO_SUPERVISOR_NAME 
+                + NanoSatMOFrameworkProvider.NANOSAT_MO_SUPERVISOR_NAME
                 + File.separator
                 + FILENAME_CENTRAL_DIRECTORY_SERVICE;
 
@@ -250,7 +261,8 @@ public final class NanoSatMOConnectorImpl extends NanoSatMOFrameworkProvider {
                 Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.SEVERE, null, ex);
             }
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.WARNING, "The File " + file.getPath() + " could not be found!");
+            Logger.getLogger(NanoSatMOConnectorImpl.class.getName()).log(Level.WARNING, 
+                    "The File " + file.getPath() + " could not be found!");
             return null;
         }
 
