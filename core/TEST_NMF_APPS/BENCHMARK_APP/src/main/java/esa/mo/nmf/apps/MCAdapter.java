3c2df4022579f979f19bddcbd8e852e63772a94f
Cesar Coelho <cesar.coelho@esa.int>
2017-08-07 13:25:49 +0200

    [core] Benchmark App now can handle the store of aggregations.
    Former-commit-id: 6ce05dcf258e4297f3eea769b186995ec34e137b [formerly 88cee1262ccbabfebc1bd4b22e5e5dc5a289d1c1]
    Former-commit-id: a3d4f0317f9cc6865fca6477b46aada5650aac2c

--- core/TEST_NMF_APPS/BENCHMARK_APP/src/main/java/esa/mo/nmf/apps/MCAdapter.java
+++ core/TEST_NMF_APPS/BENCHMARK_APP/src/main/java/esa/mo/nmf/apps/MCAdapter.java
@@ -39,13 +39,19 @@ import org.ccsds.moims.mo.mal.structures.IdentifierList;
 import org.ccsds.moims.mo.mal.structures.LongList;
 import org.ccsds.moims.mo.mal.structures.UInteger;
 import org.ccsds.moims.mo.mal.structures.UOctet;
+import org.ccsds.moims.mo.mal.structures.UShort;
+import org.ccsds.moims.mo.mc.action.structures.ActionDefinitionDetails;
+import org.ccsds.moims.mo.mc.action.structures.ActionDefinitionDetailsList;
 import org.ccsds.moims.mo.mc.aggregation.AggregationHelper;
 import org.ccsds.moims.mo.mc.aggregation.structures.AggregationDefinitionDetails;
 import org.ccsds.moims.mo.mc.aggregation.structures.AggregationDefinitionDetailsList;
 import org.ccsds.moims.mo.mc.aggregation.structures.AggregationParameterSet;
 import org.ccsds.moims.mo.mc.aggregation.structures.AggregationParameterSetList;
 import org.ccsds.moims.mo.mc.parameter.structures.ParameterRawValueList;
+import org.ccsds.moims.mo.mc.structures.ArgumentDefinitionDetails;
+import org.ccsds.moims.mo.mc.structures.ArgumentDefinitionDetailsList;
 import org.ccsds.moims.mo.mc.structures.AttributeValueList;
+import org.ccsds.moims.mo.mc.structures.ConditionalConversionList;
 
 /**
  *
@@ -60,7 +66,34 @@ public class MCAdapter extends MonitorAndControlNMFAdapter {
 
     @Override
     public void initialRegistrations(MCRegistration registration) {
+        registration.setMode(MCRegistration.RegistrationMode.DONT_UPDATE_IF_EXISTS);
 
+        // ------------------ Actions ------------------
+        ActionDefinitionDetailsList actionDefs = new ActionDefinitionDetailsList();
+        IdentifierList actionNames = new IdentifierList();
+
+        ArgumentDefinitionDetailsList arguments1 = new ArgumentDefinitionDetailsList();
+        {
+            Byte rawType = Attribute._DOUBLE_TYPE_SHORT_FORM;
+            String rawUnit = "KB";
+            ConditionalConversionList conditionalConversions = null;
+            Byte convertedType = null;
+            String convertedUnit = null;
+
+            arguments1.add(new ArgumentDefinitionDetails(rawType, rawUnit,
+                    conditionalConversions, convertedType, convertedUnit));
+        }
+
+        actionDefs.add(new ActionDefinitionDetails(
+                "Stores " + NUMBER_OF_OBJS + " aggregation definition objects in the COM Archive.",
+                new UOctet((short) 0),
+                new UShort(0),
+                arguments1,
+                null
+        ));
+        actionNames.add(new Identifier(ACTION_STORE_AGGS));
+
+        LongList actionObjIds = registration.registerActions(actionNames, actionDefs);
     }
 
     @Override
@@ -77,39 +110,37 @@ public class MCAdapter extends MonitorAndControlNMFAdapter {
     public UInteger actionArrived(Identifier name, AttributeValueList attributeValues, Long actionInstanceObjId, boolean reportProgress, MALInteraction interaction) {
 
         if (ACTION_STORE_AGGS.equals(name.getValue())) {
-            
-        
-        try {
-            AggregationDefinitionDetailsList defs = new AggregationDefinitionDetailsList();
-            AggregationDefinitionDetails def = new AggregationDefinitionDetails();
-            def.setDescription("dfvgdf");
-            def.setCategory(new UOctet((short) 1));
-            def.setSendUnchanged(false);
-            def.setGenerationEnabled(true);
-            def.setReportInterval(new Duration(45));
-            def.setFilterEnabled(false);
-            def.setFilteredTimeout(new Duration(54));
-            def.setSendDefinitions(false);
-            AggregationParameterSetList aaa = new AggregationParameterSetList();
-            AggregationParameterSet aa = new AggregationParameterSet();
-            aa.setDomain(null);
-            LongList lissssst = new LongList();
-            lissssst.add(new Long(65));
-            aa.setParameters(lissssst);
-            aa.setSampleInterval(new Duration(43));
-            aa.setReportFilter(null);
-            aaa.add(aa);
-            def.setParameterSets(aaa);
-            for (int i = 0; i < NUMBER_OF_OBJS; i++) {
-                defs.add(def);
-            }
-            ArchiveDetailsList archDetails = HelperArchive.generateArchiveDetailsList(null, null,
-                    nanoSatMOFramework.getMCServices().getActionService().getConnectionProvider().getConnectionDetails());
-            for (int i = 0; i < NUMBER_OF_OBJS - 1; i++) {
-                archDetails.add(archDetails.get(0));
-            }
-            long startTime = System.nanoTime();
-            /*
+            try {
+                AggregationDefinitionDetailsList defs = new AggregationDefinitionDetailsList();
+                AggregationDefinitionDetails def = new AggregationDefinitionDetails();
+                def.setDescription("dfvgdf");
+                def.setCategory(new UOctet((short) 1));
+                def.setSendUnchanged(false);
+                def.setGenerationEnabled(true);
+                def.setReportInterval(new Duration(45));
+                def.setFilterEnabled(false);
+                def.setFilteredTimeout(new Duration(54));
+                def.setSendDefinitions(false);
+                AggregationParameterSetList aaa = new AggregationParameterSetList();
+                AggregationParameterSet aa = new AggregationParameterSet();
+                aa.setDomain(null);
+                LongList lissssst = new LongList();
+                lissssst.add(new Long(65));
+                aa.setParameters(lissssst);
+                aa.setSampleInterval(new Duration(43));
+                aa.setReportFilter(null);
+                aaa.add(aa);
+                def.setParameterSets(aaa);
+                for (int i = 0; i < NUMBER_OF_OBJS; i++) {
+                    defs.add(def);
+                }
+                ArchiveDetailsList archDetails = HelperArchive.generateArchiveDetailsList(null, null,
+                        nanoSatMOFramework.getMCServices().getActionService().getConnectionProvider().getConnectionDetails());
+                for (int i = 0; i < NUMBER_OF_OBJS - 1; i++) {
+                    archDetails.add(archDetails.get(0));
+                }
+                long startTime = System.nanoTime();
+                /*
             try {
             LongList objIds = nanoSatMOFramework.getCOMServices().getArchiveService().store(
             true,
@@ -143,42 +174,41 @@ public class MCAdapter extends MonitorAndControlNMFAdapter {
             Logger.getLogger(ParameterManager.class.getName()).log(Level.SEVERE, null, ex);
             }
             
-             */
-            try {
+                 */
+                try {
 
-                for (int i = 0; i < defs.size(); i++) {
-                    ArchiveDetailsList xxx = new ArchiveDetailsList();
-                    AggregationDefinitionDetailsList yyy = new AggregationDefinitionDetailsList();
-                    xxx.add(archDetails.get(0));
-                    yyy.add(defs.get(i));
+                    for (int i = 0; i < defs.size(); i++) {
+                        ArchiveDetailsList xxx = new ArchiveDetailsList();
+                        AggregationDefinitionDetailsList yyy = new AggregationDefinitionDetailsList();
+                        xxx.add(archDetails.get(0));
+                        yyy.add(defs.get(i));
 
-                    nanoSatMOFramework.getCOMServices().getArchiveService().store(
-                            true,
-                            AggregationHelper.AGGREGATIONDEFINITION_OBJECT_TYPE,
-                            nanoSatMOFramework.getMCServices().getActionService().getConnectionProvider().getConnectionDetails().getDomain(),
-                            xxx,
-                            yyy,
-                            null);
+                        nanoSatMOFramework.getCOMServices().getArchiveService().store(
+                                true,
+                                AggregationHelper.AGGREGATIONDEFINITION_OBJECT_TYPE,
+                                nanoSatMOFramework.getMCServices().getActionService().getConnectionProvider().getConnectionDetails().getDomain(),
+                                xxx,
+                                yyy,
+                                null);
 
+                    }
+
+                } catch (MALException ex) {
+                    Logger.getLogger(ParameterManager.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (MALInteractionException ex) {
+                    Logger.getLogger(ParameterManager.class.getName()).log(Level.SEVERE, null, ex);
                 }
-
-            } catch (MALException ex) {
-                Logger.getLogger(ParameterManager.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (MALInteractionException ex) {
-                Logger.getLogger(ParameterManager.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            long estimatedTime = System.nanoTime() - startTime;
-            Logger.getLogger(BenchmarkApp.class.getName()).log(Level.INFO, "Total time: " + NUMBER_OF_OBJS + " objects in {0} nanoseconds", estimatedTime);
-            float objectPerSec = NUMBER_OF_OBJS / ((float) ((float) estimatedTime / (float) 1000000000));
-            float averageTimePerObj = 1 / objectPerSec;
-            Logger.getLogger(BenchmarkApp.class.getName()).log(Level.INFO, "Objects per second: " + objectPerSec + " (average: " + averageTimePerObj + " sec)");
+                long estimatedTime = System.nanoTime() - startTime;
+                Logger.getLogger(BenchmarkApp.class.getName()).log(Level.INFO, "Total time: " + NUMBER_OF_OBJS + " objects in {0} nanoseconds", estimatedTime);
+                float objectPerSec = NUMBER_OF_OBJS / ((float) ((float) estimatedTime / (float) 1000000000));
+                float averageTimePerObj = 1 / objectPerSec;
+                Logger.getLogger(BenchmarkApp.class.getName()).log(Level.INFO, "Objects per second: " + objectPerSec + " (average: " + averageTimePerObj + " sec)");
 
 //        nanoSatMOFramework.getCOMServices().getArchiveService().reset();
-        } catch (NMFException ex) {
-            Logger.getLogger(BenchmarkApp.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NMFException ex) {
+                Logger.getLogger(BenchmarkApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-                }
-
 
         return null;  // Action service not integrated
     }
