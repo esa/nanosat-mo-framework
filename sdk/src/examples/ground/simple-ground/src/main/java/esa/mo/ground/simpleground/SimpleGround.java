3de7cf5591c9e61e34aa10fa6452e77832c2a1f0
Dominik Marszk <dominik.marszk@esa.int>
2018-03-12 21:54:03 +0100

    [sdk] Refactor artifacts naming, versioning and hierarchy
    - Change base groupId for NMF artifacts to int.esa.nmf
    This creates a clear distinction between CCSDS MO
      and NMF artifacts.
    
    - Rename artifacts using systematized pattern
    - Make artifact descriptions clearer
    This makes it easier for a developer to understand
      the purpose and affiliation of the artifact.
    
    - Manage dependencies versions using qualifier
    The version qualifier is [x.y.z, x.y+1.0)
      This allows releasing patched NMF components
      without the need to upgrade parent POM.
    
    - Rename NMF parent POM to int.esa.nmf.parent

---
+++ sdk/src/examples/ground/simple-ground/src/main/java/esa/mo/ground/simpleground/SimpleGround.java
@@ -0,0 +1,71 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.ground.simpleground;
+
+import esa.mo.helpertools.connections.ConnectionConsumer;
+import esa.mo.nmf.groundmoadapter.GroundMOAdapterImpl;
+import esa.mo.nmf.groundmoadapter.SimpleDataReceivedListener;
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Ground consumer: Demo Simple Ground
+ */
+public class SimpleGround {
+
+    private final GroundMOAdapterImpl gma;
+
+    public SimpleGround() {
+        ConnectionConsumer connection = new ConnectionConsumer();
+
+        try {
+            connection.loadURIs();
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(SimpleGround.class.getName()).log(Level.SEVERE,
+                    "The URIs could not be loaded.", ex);
+        }
+
+        gma = new GroundMOAdapterImpl(connection);
+        gma.addDataReceivedListener(new DataReceivedAdapter());
+    }
+
+    /**
+     * Main command line entry point.
+     *
+     * @param args the command line arguments
+     * @throws java.lang.Exception If there is an error
+     */
+    public static void main(final String args[]) throws Exception {
+        SimpleGround demo = new SimpleGround();
+    }
+
+    class DataReceivedAdapter extends SimpleDataReceivedListener {
+
+        @Override
+        public void onDataReceived(String parameterName, Serializable data) {
+            Logger.getLogger(SimpleGround.class.getName()).log(Level.INFO,
+                    "\nParameter name: {0}" + "\n" + "Data content:\n{1}",
+                    new Object[]{parameterName, data.toString()});
+        }
+    }
+}
