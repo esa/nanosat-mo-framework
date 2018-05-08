9afecd7b3f783cf722b106faeb5d3bc44b9c2e57
Dominik Marszk <dominik.marszk@esa.int>
2018-05-08 10:15:16 +0200

    [sdk] Moved both doc and setting files to common "resources" directory
    Renamed sdk-assembly to sdk-package

--- sdk/Software_Simulator_Client/runSoftwareSimulatorClient.bat
+++
@@ -1,2 +0,0 @@
-cd ..
-java -cp Playground\libs\NanoSat_MO_Framework\NMF_LIBRARY_SOFTWARE_SIMULATOR-jar-with-dependencies.jar opssat.simulator.main.MainClient
--- sdk/sdk-assembly/src/mainDirectory/bin/examples/nanosat-mo-supervisor/runSupervisor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ${0%/*}
-java -classpath "./../../libs/NanoSat_MO_Framework/*" esa.mo.nmf.provider.NanoSatMOSupervisorSoftSimImpl
