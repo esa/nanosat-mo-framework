dff47ab99a968a1a6350c0f2f8ca2f58ae52e8dc
Yannick Lavan <yannick.lavan@esa.int>
2019-05-23 15:18:59 +0000

    Feature #426: Tests for spacecraft sim
    Generate regression tests for opssat-spacevraft-
    simulator.

---
+++ mission/simulator/opssat-spacecraft-simulator/src/test/java/esa/nmf/test/RegressionTest.java
@@ -0,0 +1,10 @@
+package esa.nmf.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class, RegressionTest1.class, RegressionTest2.class })
+public class RegressionTest {
+}
+
