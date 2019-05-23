e4dcc192ffbb1b32486d31c67e8617cc38c9c111
Yannick Lavan <yannick.lavan@esa.int>
2019-05-23 13:04:43 +0000

    Feature #426: Tests for com-impl
    Implement regression tests for ccsds-com-impl
     subproject.

---
+++ core/mo-services-impl/ccsds-com-impl/src/test/java/esa/nmf/test/RegressionTest.java
@@ -0,0 +1,10 @@
+package esa.nmf.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTest0.class, RegressionTest1.class, RegressionTest2.class, RegressionTest3.class, RegressionTest4.class, RegressionTest5.class })
+public class RegressionTest {
+}
+
