fff6307e4360fa255b766c17b387abd357669210
Yannick Lavan <yannick.lavan@esa.int>
2019-05-23 15:06:04 +0000

    Feature #426: Tests for opssat-gps-simulator
    Generate regression tests for opssat-gps-simulator
    subproject.

---
+++ mission/simulator/opssat-gps-simulator/src/test/java/esa/nmf/test/RegressionTest0.java
@@ -0,0 +1,83 @@
+package esa.nmf.test;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RegressionTest0 {
+
+    public static boolean debug = false;
+
+    @Test
+    public void test1() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test1");
+        java.math.BigDecimal bigDecimal2 = opssat.simulator.GPS.truncateDecimal((double) ' ', (int) ' ');
+        org.junit.Assert.assertNotNull(bigDecimal2);
+    }
+
+    @Test
+    public void test2() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test2");
+        opssat.simulator.Vector vector3 = new opssat.simulator.Vector((double) (byte) 10, 0.0d, (double) (byte) 0);
+        double double4 = vector3.z();
+        double double5 = vector3.x();
+        double double6 = vector3.x();
+        double double7 = vector3.x();
+        org.junit.Assert.assertTrue("'" + double4 + "' != '" + 0.0d + "'", double4 == 0.0d);
+        org.junit.Assert.assertTrue("'" + double5 + "' != '" + 10.0d + "'", double5 == 10.0d);
+        org.junit.Assert.assertTrue("'" + double6 + "' != '" + 10.0d + "'", double6 == 10.0d);
+        org.junit.Assert.assertTrue("'" + double7 + "' != '" + 10.0d + "'", double7 == 10.0d);
+    }
+
+    @Test
+    public void test3() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test3");
+        opssat.simulator.InstrumentsSimulator instrumentsSimulator0 = new opssat.simulator.InstrumentsSimulator();
+        instrumentsSimulator0.printRealPosition();
+        int int2 = instrumentsSimulator0.getMode();
+        org.junit.Assert.assertTrue("'" + int2 + "' != '" + 0 + "'", int2 == 0);
+    }
+
+    @Test
+    public void test4() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test4");
+        opssat.simulator.Camera camera0 = new opssat.simulator.Camera();
+        byte[] byteArray2 = camera0.takePicture(0.0f);
+        org.junit.Assert.assertNull(byteArray2);
+    }
+
+    @Test
+    public void test5() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test5");
+        opssat.simulator.InstrumentsSimulator instrumentsSimulator0 = new opssat.simulator.InstrumentsSimulator();
+        instrumentsSimulator0.printRealPosition();
+        double double2 = instrumentsSimulator0.getFineADCSmagnetometerBtheta();
+// flaky:         org.junit.Assert.assertTrue("'" + double2 + "' != '" + 1.1814262328124097E-5d + "'", double2 == 1.1814262328124097E-5d);
+    }
+
+    @Test
+    public void test6() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test6");
+        opssat.simulator.Vector vector6 = new opssat.simulator.Vector((double) (byte) 10, 0.0d, (double) (byte) 0);
+        double double7 = vector6.x();
+        java.util.Date date8 = null;
+        opssat.simulator.Orbit.OrbitParameters orbitParameters9 = new opssat.simulator.Orbit.OrbitParameters((double) 'a', (double) 7, (double) 6, vector6, date8);
+        org.junit.Assert.assertTrue("'" + double7 + "' != '" + 10.0d + "'", double7 == 10.0d);
+    }
+
+    @Test
+    public void test7() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test7");
+        opssat.simulator.InstrumentsSimulator instrumentsSimulator0 = new opssat.simulator.InstrumentsSimulator();
+        java.lang.Boolean boolean2 = instrumentsSimulator0.takePicture((int) (short) 100);
+        org.junit.Assert.assertTrue("'" + boolean2 + "' != '" + false + "'", boolean2.equals(false));
+    }
+}
