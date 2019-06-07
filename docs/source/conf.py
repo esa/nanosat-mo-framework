e63842fbbc65e71ceacc4f4c1df99f7c40105e09
Yannick Lavan <yannick.lavan@esa.int>
2019-06-07 16:09:16 +0000

    Feature #465: Use current version of sphinx
    Use current version of sphinx to avoid bugs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,6 @@ author = 'Dominik Marszk, Yannick Lavan'
 
 # The full version, including alpha/beta/rc tags
 release = '2.0.0-SNAPSHOT'
-master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
