3aafe11ef26742b5662cbc124a71b08d0934a204
Yannick Lavan <yannick.lavan@esa.int>
2019-06-28 11:48:03 +0000

    Bug #495: Add data structure for platform config values

---
+++ mission/simulator/opssat-spacecraft-simulator/src/main/java/opssat/simulator/util/PlatformMessage.java
@@ -0,0 +1,30 @@
+package opssat.simulator.util;
+
+import java.io.Serializable;
+
+public class PlatformMessage implements Serializable {
+  private String key;
+  private String value;
+
+  public PlatformMessage(String key, String value) {
+    super();
+    this.key = key;
+    this.value = value;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("PlatformMessage { ");
+    sb.append("Key: ").append(this.key).append(", Value: ").append(this.value);
+    sb.append("};");
+    return sb.toString();
+  }
+}
