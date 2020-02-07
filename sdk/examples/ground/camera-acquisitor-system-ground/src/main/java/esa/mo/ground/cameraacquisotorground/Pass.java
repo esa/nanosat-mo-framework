06d36398a71a4141caed8bc4fc9d97c16f112459
Kevin Otto <Kevin@KevinOtto.de>
2020-02-07 13:16:25 +0000

    [camera acquisitor ground] Add class for handling passes

---
+++ sdk/examples/ground/camera-acquisitor-system-ground/src/main/java/esa/mo/ground/cameraacquisotorground/Pass.java
@@ -0,0 +1,99 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.ground.cameraacquisotorground;
+
+import esa.mo.nmf.apps.CameraAcquisitorSystemMCAdapter;
+import org.hipparchus.ode.events.Action;
+import org.orekit.propagation.SpacecraftState;
+import org.orekit.propagation.events.BooleanDetector;
+import org.orekit.propagation.events.handlers.EventHandler;
+import org.orekit.time.AbsoluteDate;
+
+/**
+ *
+ * @author Kevin Otto <Kevin@KevinOtto.de>
+ */
+class Pass implements EventHandler<BooleanDetector>
+{
+
+  private final AbsoluteDate notBeforeDate;
+  private final long worstCaseRotationTimeSeconds;
+
+  private AbsoluteDate passStart;
+  private AbsoluteDate passEnd;
+  private AbsoluteDate optimalTime;
+  private boolean timeFound = false;
+
+  private boolean startIsSet = false;
+
+  public Pass(AbsoluteDate notBeforeDate, long worstCaseRotationTimeSeconds)
+  {
+    this.notBeforeDate = notBeforeDate;
+    this.worstCaseRotationTimeSeconds = worstCaseRotationTimeSeconds;
+  }
+
+  public AbsoluteDate getPassStart()
+  {
+    return passStart;
+  }
+
+  public AbsoluteDate getPassEnd()
+  {
+    return passEnd;
+  }
+
+  public AbsoluteDate getOptimalTime()
+  {
+    return optimalTime;
+  }
+
+  public boolean isTimeFound()
+  {
+    return timeFound;
+  }
+
+  @Override
+  public Action eventOccurred(SpacecraftState s, BooleanDetector detector, boolean increasing)
+  {
+    if (increasing) {
+      if (!startIsSet) {
+        this.passStart = s.getDate();
+        startIsSet = true;
+      }
+    } else {
+      this.passEnd = s.getDate();
+      double elepsedTime = this.passEnd.durationFrom(this.passStart);
+      this.optimalTime = this.passStart.shiftedBy(elepsedTime / 2);
+
+      if (this.optimalTime.durationFrom(CameraAcquisitorSystemMCAdapter.getNow()) <= worstCaseRotationTimeSeconds && this.optimalTime.compareTo(
+          this.notBeforeDate) > 0) {
+
+        //time to close, try again
+        startIsSet = false;
+        return Action.CONTINUE;
+      }
+      // time found
+      timeFound = true;
+      return Action.STOP;
+    }
+    return Action.CONTINUE;
+  }
+}
