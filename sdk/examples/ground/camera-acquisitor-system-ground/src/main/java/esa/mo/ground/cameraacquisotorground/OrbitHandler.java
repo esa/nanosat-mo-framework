0805edda9502e4f7297fad6efd3d4457f99ecd35
Kevin Otto <Kevin@KevinOtto.de>
2020-02-07 13:16:52 +0000

    [camera acquisitor ground] Implement pass handling and orbit propagation

--- sdk/examples/ground/camera-acquisitor-system-ground/src/main/java/esa/mo/ground/cameraacquisotorground/OrbitHandler.java
+++ sdk/examples/ground/camera-acquisitor-system-ground/src/main/java/esa/mo/ground/cameraacquisotorground/OrbitHandler.java
@@ -20,19 +20,34 @@
  */
 package esa.mo.ground.cameraacquisotorground;
 
-import esa.mo.nmf.sdk.OrekitResources;
+import esa.mo.nmf.apps.CameraAcquisitorSystemMCAdapter;
+import esa.mo.nmf.apps.CameraAcquisitorSystemTargetLocation;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.TimerTask;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import org.hipparchus.ode.events.Action;
+import org.hipparchus.util.FastMath;
+import org.orekit.bodies.CelestialBodyFactory;
 import org.orekit.bodies.GeodeticPoint;
 import org.orekit.bodies.OneAxisEllipsoid;
-import org.orekit.data.DataProvidersManager;
 import org.orekit.frames.FactoryManagedFrame;
 import org.orekit.frames.FramesFactory;
+import org.orekit.frames.TopocentricFrame;
+import org.orekit.models.earth.EarthITU453AtmosphereRefraction;
 import org.orekit.propagation.SpacecraftState;
 import org.orekit.propagation.analytical.tle.TLE;
 import org.orekit.propagation.analytical.tle.TLEPropagator;
+import org.orekit.propagation.events.BooleanDetector;
+import org.orekit.propagation.events.ElevationDetector;
+import org.orekit.propagation.events.EventDetector;
+import org.orekit.propagation.events.GroundAtNightDetector;
+import org.orekit.propagation.events.handlers.EventHandler;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.utils.Constants;
 import org.orekit.utils.IERSConventions;
+import org.orekit.utils.PVCoordinatesProvider;
 
 /**
  *
@@ -40,6 +55,20 @@ import org.orekit.utils.IERSConventions;
  */
 public class OrbitHandler
 {
+
+  public static enum TimeModeEnum
+  {
+    ANY, DAYTIME, NIGHTTIME
+  }
+
+  private TreeSet targets = new TreeSet(new Comparator<PositionAndTime>()
+  {
+    @Override
+    public int compare(PositionAndTime o1, PositionAndTime o2)
+    {
+      return o1.date.compareTo(o2.date);
+    }
+  });
 
   private final FactoryManagedFrame earthFrame;
   private final OneAxisEllipsoid earth;
@@ -110,6 +139,46 @@ public class OrbitHandler
     System.out.println(propagator.getInitialState().getDate());
     return earth.transform(
         finalState.getPVCoordinates(earthFrame).getPosition(), earthFrame, finalState.getDate());
+  }
+
+  public Pass getPassTime(double longitude, double latitude, double maxAngle,
+      TimeModeEnum timeMode, AbsoluteDate notBeforeDate, long worstCaseRotationTimeSeconds,
+      long simulationRange)
+  {
+
+    GeodeticPoint targetLocation = new GeodeticPoint(latitude, longitude, 0);
+    TopocentricFrame groundFrame = new TopocentricFrame(earth, targetLocation, "cameraTarget");
+
+    // ------------------ create Detectors ------------------
+    EventDetector overpassDetector = new ElevationDetector(groundFrame)
+        .withConstantElevation(FastMath.toRadians(90.0 - maxAngle));
+
+    Pass pass = new Pass(notBeforeDate, worstCaseRotationTimeSeconds);
+    if (timeMode != TimeModeEnum.ANY) {
+      PVCoordinatesProvider sun = CelestialBodyFactory.getSun(); //create detector for nighttime
+
+      EventDetector timeModeDetector = new GroundAtNightDetector(groundFrame, sun, latitude,
+          new EarthITU453AtmosphereRefraction(0));
+
+      //invert nightTime detector if photograph should be taken at daytime
+      if (timeMode == TimeModeEnum.DAYTIME) {
+        timeModeDetector = BooleanDetector.notCombine(timeModeDetector);
+      }
+      overpassDetector = BooleanDetector.andCombine(timeModeDetector, overpassDetector)
+          .withHandler(pass);
+    } else {
+      overpassDetector = BooleanDetector.orCombine(overpassDetector)
+          .withHandler(pass);
+    }
+
+    propagator.addEventDetector(overpassDetector);
+
+    AbsoluteDate endDate = notBeforeDate.shiftedBy(simulationRange);
+    SpacecraftState finalState = propagator.propagate(endDate);
+
+    propagator.clearEventsDetectors();
+
+    return pass;
   }
 
 }
