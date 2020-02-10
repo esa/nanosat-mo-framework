20ac567d0e881e2b9314e9eaa0fb170ecc282d25
Kevin Otto <Kevin@KevinOtto.de>
2020-02-10 10:50:58 +0000

    [camera acquisitor ground] Add Spring framework for REST API

---
+++ sdk/examples/ground/camera-acquisitor-system-ground/src/main/java/esa/mo/ground/cameraacquisotorground/SpringInitiator.java
@@ -0,0 +1,50 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.ground.cameraacquisotorground;
+
+import esa.mo.ground.cameraacquisotorground.CameraAcquisitorGround;
+import java.util.Collections;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Kevin Otto <Kevin@KevinOtto.de>
+ */
+@SpringBootApplication
+public class SpringInitiator
+{
+
+  /**
+   * Main command line entry point.
+   *
+   * @param args the command line arguments
+   * @throws java.lang.Exception If there is an error
+   */
+  public static void main(final String args[])
+  {
+    SpringApplication app = new SpringApplication(CameraAcquisitorGround.class);
+
+    app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+    app.run(args);
+  }
+
+}
---
+++ sdk/examples/ground/camera-acquisitor-system-ground/src/main/java/esa/mo/ground/restservice/GroundTrack.java
@@ -0,0 +1,49 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.ground.restservice;
+
+/**
+ *
+ * @author Kevin Otto <Kevin@KevinOtto.de>
+ */
+public class GroundTrack
+{
+
+  private final long id;
+  private final PositionAndTime[] trackPoints;
+
+  public GroundTrack(long id, PositionAndTime[] track)
+  {
+    this.id = id;
+    this.trackPoints = track;
+  }
+
+  public long getId()
+  {
+    return id;
+  }
+
+  public PositionAndTime[] getTrack()
+  {
+    return trackPoints;
+  }
+
+}
