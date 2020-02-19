0c1bf9c9c8d0ca03ba5f9897a1a0906538b6fe43
Kevin Otto <Kevin@KevinOtto.de>
2020-02-19 10:44:30 +0000

    [GroundMOAdapterImpl] Make action-instance-ID unique

--- core/nmf-composites/ground-mo-adapter/src/main/java/esa/mo/nmf/groundmoadapter/SimpleCommandingInterface.java
+++ core/nmf-composites/ground-mo-adapter/src/main/java/esa/mo/nmf/groundmoadapter/SimpleCommandingInterface.java
@@ -25,61 +25,57 @@ import java.io.Serializable;
 import org.ccsds.moims.mo.mc.structures.AttributeValueList;
 
 /**
- * The SimpleCommandingInterface interface provides a simpler way to send
- * commands to a NanoSat MO Framework-based provider. Hides the complexity of
- * the MO services by providing direct Java data types injection.
+ * The SimpleCommandingInterface interface provides a simpler way to send commands to a NanoSat MO
+ * Framework-based provider. Hides the complexity of the MO services by providing direct Java data
+ * types injection.
  *
  * @author Cesar Coelho
  */
-public interface SimpleCommandingInterface {
+public interface SimpleCommandingInterface
+{
 
-    /**
-     * The setParameter method allows an external software entity to send Data
-     * to the NanoSat MO Framework provider using the Parameter service. If
-     * there is no parameter definition with the submitted name, the method
-     * shall automatically create the parameter definition in the Parameter
-     * service. Any sort of data can be exchanged as long as the content is
-     * serializable.
-     *
-     * @param parameterName The name of the Parameter as set in the parameter
-     * definition
-     * @param content The content of the Parameter
-     */
-    void setParameter(String parameterName, Serializable content);
+  /**
+   * The setParameter method allows an external software entity to send Data to the NanoSat MO
+   * Framework provider using the Parameter service. If there is no parameter definition with the
+   * submitted name, the method shall automatically create the parameter definition in the Parameter
+   * service. Any sort of data can be exchanged as long as the content is serializable.
+   *
+   * @param parameterName The name of the Parameter as set in the parameter definition
+   * @param content       The content of the Parameter
+   */
+  void setParameter(String parameterName, Serializable content);
 
-    /**
-     * The addDataReceivedListener method allows an external software entity to
-     * submit a DataReceivedListener in order to receive Data from the NanoSat
-     * MO Framework provider using the Parameter service.
-     *
-     * @param dataReceivedListener The Listener where the data will be received
-     */
-    void addDataReceivedListener(DataReceivedListener dataReceivedListener);
+  /**
+   * The addDataReceivedListener method allows an external software entity to submit a
+   * DataReceivedListener in order to receive Data from the NanoSat MO Framework provider using the
+   * Parameter service.
+   *
+   * @param dataReceivedListener The Listener where the data will be received
+   */
+  void addDataReceivedListener(DataReceivedListener dataReceivedListener);
 
-    /**
-     * The invokeAction method allows an external software entity to submit an
-     * Action to the NanoSat MO Framework provider just by selecting the name of
-     * the action and the respective implementer-specific data necessary for the
-     * execution of that action.
-     *
-     * @param actionName The name of the Action to be executed
-     * @param objects Implementer-specific data necessary to execute the action
-     */
-    void invokeAction(String actionName, Serializable[] objects);
+  /**
+   * The invokeAction method allows an external software entity to submit an Action to the NanoSat
+   * MO Framework provider just by selecting the name of the action and the respective
+   * implementer-specific data necessary for the execution of that action.
+   *
+   * @param actionName The name of the Action to be executed
+   * @param objects    Implementer-specific data necessary to execute the action
+   */
+  Long invokeAction(String actionName, Serializable[] objects);
 
-    /**
-     * The invokeAction method allows an external software entity to invoke an
-     * action using the Action service by selecting the defInstId of
-     * the action and the respective argument values necessary for the
-     * execution of that action.
-     *
-     * @param defInstId The object instance identifier of the ActionDefinition
-     * @param argumentValues List containing the values of the arguments. The
-     * ordering of the list matches that of the definition
-     * @return The object instance identifier of the ActionInstance. This can be
-     * used to track the action via the Activity Tracking service.
-     * @throws NMFException in case something goes wrong
-     */
-    Long invokeAction(Long defInstId, AttributeValueList argumentValues) throws NMFException;
+  /**
+   * The invokeAction method allows an external software entity to invoke an action using the Action
+   * service by selecting the defInstId of the action and the respective argument values necessary
+   * for the execution of that action.
+   *
+   * @param defInstId      The object instance identifier of the ActionDefinition
+   * @param argumentValues List containing the values of the arguments. The ordering of the list
+   *                       matches that of the definition
+   * @return The object instance identifier of the ActionInstance. This can be used to track the
+   * action via the Activity Tracking service.
+   * @throws NMFException in case something goes wrong
+   */
+  Long invokeAction(Long defInstId, AttributeValueList argumentValues) throws NMFException;
 
 }
