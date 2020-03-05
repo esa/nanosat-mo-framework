e945fd014718e1ffea50518875f78f263806726c
Kevin Otto <Kevin@KevinOtto.de>
2020-03-05 16:00:22 +0000

    [Annotations] Bug #354 Add Annotations for Action ActionParameter and Parameter

---
+++ core/nmf-composites/generic-composite-model/src/main/java/esa/mo/nmf/annotations/ActionParameter.java
@@ -0,0 +1,48 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2015      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under the European Space Agency Public License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
+package esa.mo.nmf.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.ccsds.moims.mo.mc.structures.ConditionalConversionList;
+
+/**
+ *
+ * @author Kevin Otto <Kevin@KevinOtto.de>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface ActionParameter
+{
+
+  String name() default "";
+
+  String description() default "";
+
+  byte rawType() default 0;
+
+  String rawUnit() default "";
+  ConditionalConversionList conditionalConversions = null;
+  Byte convertedType = null;
+  String convertedUnit = null;
+}
