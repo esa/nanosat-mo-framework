77bae8850bbcec6c2a3e7c7ea8a2f4938e4cfff7
Kevin Otto <Kevin@KevinOtto.de>
2020-03-05 17:33:36 +0000

    [Annotations] Add Tests for Annotations

--- core/nmf-composites/generic-composite-model/src/test/java/esa/nmf/test/RegressionTest.java
+++ core/nmf-composites/generic-composite-model/src/test/java/esa/nmf/test/RegressionTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ RegressionTest0.class, RegressionTest1.class })
-public class RegressionTest {
+@Suite.SuiteClasses({RegressionTest0.class, RegressionTest1.class, ParameterTest.class})
+public class RegressionTest
+{
 }
-
