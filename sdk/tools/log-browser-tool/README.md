8919742906b265e67736f65942132e41fccaf1a8
Tanguy Soto <tanguy.soto@esa.int>
2021-03-01 16:32:44 +0100

    feat: introduce log-browser-tool

---
+++ sdk/tools/log-browser-tool/README.md
@@ -0,0 +1,3 @@
+LOG Browser
+===========
+TODO
---
+++ sdk/tools/log-browser-tool/src/main/java/esa/mo/nmf/log_browser/LogBrowser.java
@@ -0,0 +1,22 @@
+// (C) 2021 European Space Agency
+// European Space Operations Centre
+// Darmstadt, Germany
+
+package esa.mo.nmf.log_browser;
+
+/**
+ * TODO LogBrowser
+ *
+ * @author Tanguy Soto
+ */
+public class LogBrowser {
+
+  /**
+   * Main command line entry point.
+   *
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("COMING SOON");
+  }
+}
