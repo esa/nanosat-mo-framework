5bf556a258d18ac8a8512ce6b83fe8fcbf993726
Cesar Coelho <cbwhittle@hotmail.com>
2021-04-15 14:45:32 +0200

    [pictureprocessor] Code cleanup. Changed directory where the pictures are stored

--- sdk/examples/space/picture-processor/src/main/java/esa/mo/nmf/apps/pictureprocessor/PictureProcessorApp.java
+++ sdk/examples/space/picture-processor/src/main/java/esa/mo/nmf/apps/pictureprocessor/PictureProcessorApp.java
@@ -1,11 +1,10 @@
-/*
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2021      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
  * ----------------------------------------------------------------------------
- * Copyright (C) 2021 European Space Agency
- * European Space Operations Centre
- * Darmstadt
- * Germany
- * ----------------------------------------------------------------------------
- * System : ESA NanoSat MO Framework
+ * System                : ESA NanoSat MO Framework
  * ----------------------------------------------------------------------------
  * Licensed under European Space Agency Public License (ESA-PL) Weak Copyleft – v2.4
  * You may not use this file except in compliance with the License.
@@ -14,13 +13,14 @@
  * You on an "as is" basis and without warranties of any kind, including without
  * limitation merchantability, fitness for a particular purpose, absence of
  * defects or errors, accuracy or non-infringement of intellectual property rights.
- *
+ * 
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License. 
  * ----------------------------------------------------------------------------
  */
 package esa.mo.nmf.apps.pictureprocessor;
 
+import esa.mo.nmf.AppStorage;
 import static esa.mo.nmf.apps.pictureprocessor.utils.FileUtils.createDirectoriesIfNotExist;
 
 import java.nio.file.Path;
@@ -28,17 +28,19 @@ import java.nio.file.Paths;
 
 import esa.mo.nmf.apps.pictureprocessor.mo.PictureProcessorMCAdapter;
 import esa.mo.nmf.nanosatmoconnector.NanoSatMOConnectorImpl;
+import java.io.File;
 
 /**
- * An NMF App that that takes pictures and processes them through a python script
+ * An NMF App that that takes pictures and processes them through a python
+ * script
  */
 public class PictureProcessorApp {
 
     public PictureProcessorApp() {
-        Path outputFolder = createDirectoriesIfNotExist(Paths.get("pictures"));
-
         NanoSatMOConnectorImpl connector = new NanoSatMOConnectorImpl();
-
+        File userdata = AppStorage.getAppUserdataDir();
+        String path = userdata + File.separator + "pictures";
+        Path outputFolder = createDirectoriesIfNotExist(Paths.get(path));
         connector.init(new PictureProcessorMCAdapter(connector, outputFolder));
     }
 
@@ -49,7 +51,7 @@ public class PictureProcessorApp {
      * @throws java.lang.Exception If there is an error
      */
     public static void main(final String[] args) throws Exception {
-        new PictureProcessorApp();
+        PictureProcessorApp pictureProcessorApp = new PictureProcessorApp();
     }
 
 }
--- sdk/examples/space/picture-processor/src/main/java/esa/mo/nmf/apps/pictureprocessor/process/ProcessEventListener.java
+++ sdk/examples/space/picture-processor/src/main/java/esa/mo/nmf/apps/pictureprocessor/process/ProcessEventListener.java
@@ -1,11 +1,10 @@
-/*
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2021      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
  * ----------------------------------------------------------------------------
- * Copyright (C) 2021 European Space Agency
- * European Space Operations Centre
- * Darmstadt
- * Germany
- * ----------------------------------------------------------------------------
- * System : ESA NanoSat MO Framework
+ * System                : ESA NanoSat MO Framework
  * ----------------------------------------------------------------------------
  * Licensed under European Space Agency Public License (ESA-PL) Weak Copyleft – v2.4
  * You may not use this file except in compliance with the License.
@@ -14,9 +13,9 @@
  * You on an "as is" basis and without warranties of any kind, including without
  * limitation merchantability, fitness for a particular purpose, absence of
  * defects or errors, accuracy or non-infringement of intellectual property rights.
- *
+ * 
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License. 
  * ----------------------------------------------------------------------------
  */
 package esa.mo.nmf.apps.pictureprocessor.process;
