c19cecedde8e384b8004e6dd532e3e4aee292aea
Cesar Coelho <cbwhittle@hotmail.com>
2021-09-14 11:48:26 +0200

    The AppStorage class is now setting the correct permissions during the creation of the storage folders

--- core/mo-services-impl/nmf-software-management-impl/src/main/java/esa/mo/sm/impl/util/OSValidator.java
+++ core/mo-services-impl/nmf-software-management-impl/src/main/java/esa/mo/sm/impl/util/OSValidator.java
@@ -29,19 +29,6 @@ public class OSValidator {
     private final String OS = System.getProperty("os.name").toLowerCase();
 
     public OSValidator() {
-        /*
-        if (isWindows()) {
-            System.out.println("This is Windows");
-        } else if (isMac()) {
-            System.out.println("This is Mac");
-        } else if (isUnix()) {
-            System.out.println("This is Unix or Linux");
-        } else if (isSolaris()) {
-            System.out.println("This is Solaris");
-        } else {
-            System.out.println("Your OS is not support!!");
-        }
-         */
     }
 
     public boolean isWindows() {
