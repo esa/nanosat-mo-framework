8cd888e92d133a6109714e66d52a6ead64971848
Marcel Mikołajko <marcel.mikolajko@asseco.pl>
2021-12-02 11:51:38 +0100

    Issue #1174 [ctt] Replace file header
    https://gitlab.com/esa/NMF/nmf-issues/-/issues/1174

--- sdk/tools/consumer-test-tool/src/main/java/esa/mo/nmf/ctt/utils/LoginDialog.java
+++ sdk/tools/consumer-test-tool/src/main/java/esa/mo/nmf/ctt/utils/LoginDialog.java
@@ -1,16 +1,23 @@
-//------------------------------------------------------------------------------
-//
-// System : ccsds-common
-//
-// Sub-System : esa.mo.nmf.ctt.utils
-//
-// File Name : LoginDialog.java
-//
-// Author : marcel.mikolajko
-//
-// Creation Date : 23.11.2021
-//
-//------------------------------------------------------------------------------
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2021      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under European Space Agency Public License (ESA-PL) Weak Copyleft – v2.4
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
 package esa.mo.nmf.ctt.utils;
 
 import esa.mo.com.impl.consumer.ArchiveConsumerServiceImpl;
