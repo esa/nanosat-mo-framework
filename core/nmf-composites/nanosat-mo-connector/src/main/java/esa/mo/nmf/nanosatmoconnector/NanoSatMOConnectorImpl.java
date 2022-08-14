c9832707be148a0dbe94dd933c36f6157522e4fd
Cesar Coelho <cbwhittle@hotmail.com>
2022-08-14 14:52:26 +0200

    Updates the Banner code to be printed via the LOGGER instead of the System.out.println

--- core/nmf-composites/nanosat-mo-connector/src/main/java/esa/mo/nmf/nanosatmoconnector/NanoSatMOConnectorImpl.java
+++ core/nmf-composites/nanosat-mo-connector/src/main/java/esa/mo/nmf/nanosatmoconnector/NanoSatMOConnectorImpl.java
@@ -96,25 +96,7 @@ public class NanoSatMOConnectorImpl extends NMFProvider {
     @Override
     public void init(final MonitorAndControlNMFAdapter mcAdapter) {
         super.startTime = System.currentTimeMillis();
-
-        Properties p = System.getProperties();
-        StringBuilder java = new StringBuilder();
-        java.append(p.getProperty("java.runtime.name", "?"));
-        java.append(" (version: ");
-        java.append(p.getProperty("java.runtime.version", "?"));
-        java.append(")");
-        
-        StringBuilder os = new StringBuilder();
-        os.append(p.getProperty("os.name", "?"));
-        os.append(" (version: ");
-        os.append(p.getProperty("os.version", "?"));
-        os.append(")");
-        
-        System.out.println(SEPARATOR);
-        System.out.println("NanoSat MO Framework");
-        System.out.println("OS: " + os.toString());
-        System.out.println("Java: " + java.toString());
-        System.out.println(SEPARATOR);
+        LOGGER.log(Level.INFO, this.generateStartBanner());
         
         HelperMisc.loadPropertiesFile(); // Loads: provider.properties; settings.properties; transport.properties
         ConnectionProvider.resetURILinks(); // Resets the providerURIs.properties file
