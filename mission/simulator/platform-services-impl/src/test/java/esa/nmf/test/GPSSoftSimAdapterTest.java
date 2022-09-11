b5236cb722f5c2572d5821d468df0b0d12490da9
Dominik Marszk <dominik.marszk@esa.int>
2022-09-11 13:08:05 +0000

    [tests] Update flaky GPS tests

--- mission/simulator/platform-services-impl/src/test/java/esa/nmf/test/GPSSoftSimAdapterTest.java
+++ mission/simulator/platform-services-impl/src/test/java/esa/nmf/test/GPSSoftSimAdapterTest.java
@@ -24,7 +24,7 @@ public class GPSSoftSimAdapterTest {
         //Expecting something in lines of:
         //#TIMEA,COM1,0,35.0,FINESTEERING,1337,410010.000,00000000,9924,1984;VALID,0,0,0,2005,8,25,17,53,17000,VALID*e2fc088c
         String expectedRegex = "#TIMEA,\\w{1,8}_?\\d{1,2},0,\\d{1,2}\\.\\d{1,2}," + GPS_REF_TIME_STATUS +
-                ",\\d{4},\\d{1,6}\\.\\d{3},[0|1]{8},[\\d|\\w]{4},\\d{1,5};" + CLOCK_MODEL_STATUS +
+                ",\\d{4},-?\\d{1,6}\\.\\d{3},[0|1]{8},[\\d|\\w]{4},\\d{1,5};" + CLOCK_MODEL_STATUS +
                 ",(0|\\d{1,2}\\.\\d{1,13}(e-)?(\\d{1,2})?),(0|\\d{1,2}\\.\\d{1,13}(e-)?(\\d{1,2})?)," +
                 "-?\\d{1,2}\\.\\d{11},\\d{4},\\d{1,2},\\d{1,2},\\d{1,2}," +
                 "\\d{1,2},\\d{1,5},"+ UTC_STATUS + "\\*[\\w|\\d]{1,8}";
@@ -41,7 +41,7 @@ public class GPSSoftSimAdapterTest {
         //Expecting the BESTXYZ header to be in the lines of:
         //#BESTXYZA,COM1,0,35.0,FINESTEERING,2177,306117.000,00100000,97b7,2310...
         String expectedRegex = "^#BESTXYZA,\\w{1,8}_?\\d{1,2},0,\\d{1,2}\\.\\d{1,2}," + GPS_REF_TIME_STATUS +
-                ",\\d{4},\\d{1,6}\\.\\d{3},[0-9|A-F|a-f]{8},[\\d|\\w]{4},\\d{1,5}.+";
+                ",\\d{4},-?\\d{1,6}\\.\\d{3},[0-9|A-F|a-f]{8},[\\d|\\w]{4},\\d{1,5}.+";
         String timea = gPSSoftSimAdapter.getBestXYZSentence();
         Assert.assertTrue("Actual was: " +timea, timea.matches(expectedRegex));
     }
