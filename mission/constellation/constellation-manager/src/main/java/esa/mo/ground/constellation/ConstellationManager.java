5e47ce2a8ea9f9644a98aca605ea470613fbe1e5
N Wiegand <nikolai.wiegand@gmx.de>
2022-10-16 15:22:30 +0200

    adding support for multiple nanosat segments

--- mission/constellation/constellation-manager/src/main/java/esa/mo/ground/constellation/ConstellationManager.java
+++ mission/constellation/constellation-manager/src/main/java/esa/mo/ground/constellation/ConstellationManager.java
@@ -13,111 +13,86 @@
  * You on an "as is" basis and without warranties of any kind, including without
  * limitation merchantability, fitness for a particular purpose, absence of
  * defects or errors, accuracy or non-infringement of intellectual property rights.
- * 
+ *
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  * ----------------------------------------------------------------------------
- * 
+ *
  * Author: N Wiegand (https://github.com/Klabau)
  */
 package esa.mo.ground.constellation;
 
-import esa.mo.nmf.groundmoadapter.GroundMOAdapterImpl;
-import esa.mo.nmf.commonmoadapter.SimpleDataReceivedListener;
-import org.ccsds.moims.mo.common.directory.structures.ProviderSummary;
-import org.ccsds.moims.mo.common.directory.structures.ProviderSummaryList;
-import org.ccsds.moims.mo.mal.MALException;
-import org.ccsds.moims.mo.mal.MALInteractionException;
-
 import java.io.IOException;
-import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Ground consumer: Constellation Manager
  * An orchestration tool for a constellation of NanoSatellites.
- * 
+ *
  */
-public class ConstellationManager
-{
+public class ConstellationManager {
+  private final Logger LOGGER = Logger.getLogger(
+    ConstellationManager.class.getName()
+  );
 
-  private static final String APP_PREFIX = "App: ";
-  private final Logger LOGGER = Logger.getLogger(ConstellationManager.class.getName());
-
-  private NanoSatSimulator nanoSatSimulator = null;
+  private ArrayList<NanoSatSimulator> constellation = new ArrayList<NanoSatSimulator>();
 
   /**
-   * Initialiser Constructor. 
+   * Initialiser Constructor.
    * This Class manages a constellation of NanoSatellites.
    */
-  public ConstellationManager()
-  {
-  }
+  public ConstellationManager() {}
 
   /**
    * Initializes the constellation. Creates and runs the Docker containers.
-   * 
+   *
    * Very WIP!
-   * 
+   *
    * @param name Name of the constellation. Container naming scheme: <name>-node-<1...n>
+   * @param size Constellation size
    */
-  private void InitConstellation(String name) 
-  {
-   
+  private void InitConstellation(String name, int size) {
     try {
-
-      if (this.nanoSatSimulator == null) {
-        this.nanoSatSimulator = new NanoSatSimulator(name + "-node-01");
+      if (this.constellation.size() == 0) {
+        for (int i = 1; i <= size; i++) {
+          this.constellation.add(
+              new NanoSatSimulator(name + "-node-" + i, LOGGER)
+            );
+          this.constellation.get(i - 1).run();
+        }
+      } else {
+        LOGGER.log(Level.INFO, "Constellation has already been initialized. ");
       }
 
-      this.nanoSatSimulator.run();
-
-      LOGGER.log(Level.INFO, "Initialized Container " + this.nanoSatSimulator.getName());
-    }
-    catch (IOException ex) {
-      LOGGER.log(Level.SEVERE, "Failed to run container.", ex);
+      LOGGER.log(Level.INFO, "Successfully initialized constellation. ");
+    } catch (IOException ex) {
+      LOGGER.log(Level.SEVERE, "Failed to initialize the constellation: ", ex);
     }
   }
 
   /**
    * Connects to the providers of the constellation.
-   * 
+   *
    * Very WIP!
-   * 
-   * @param providerName
+   *
    */
-  private void ConnectToProvider(String providerName) 
-  {
-    try {
-
-      LOGGER.log(Level.INFO, "Connecting to " + this.nanoSatSimulator.getDirectoryServiceURIString());
-
-      ProviderSummaryList providers = GroundMOAdapterImpl
-        .retrieveProvidersFromDirectory(this.nanoSatSimulator.getDirectoryServiceURI());
-
-      GroundMOAdapterImpl gma = null;
-      if (!providers.isEmpty()) {
-        for (ProviderSummary provider : providers) {
-          if (provider.getProviderName().toString().equals(APP_PREFIX + providerName)) {
-            gma = new GroundMOAdapterImpl(provider);
-            gma.addDataReceivedListener(new DataReceivedAdapter());
-            break;
-          }
+  private void ConnectToProviders() {
+    constellation.forEach(
+      nanoSat -> {
+        try {
+          LOGGER.log(
+            Level.INFO,
+            "Connecting to " + nanoSat.getDirectoryServiceURIString()
+          );
+          nanoSat.connectToProviders();
+        } catch (IOException ex) {
+          LOGGER.log(Level.SEVERE, "Failed to connect to provider: ", ex);
         }
       }
-
-      if(gma == null)
-      {
-        LOGGER.log(Level.SEVERE, "Failed to connect to the provider. No such provider found - " + providerName);
-      } else {
-        LOGGER.log(Level.INFO, "Successfully connected to " + providerName);
-      }
-    } catch ( MALException | MALInteractionException | IOException ex) {
-      LOGGER.log(Level.SEVERE, "Failed to connect to the provider. ", ex);
-    }
+    );
   }
-
 
   /**
    * Main command line entry point.
@@ -125,31 +100,17 @@ public class ConstellationManager
    * @param args the command line arguments
    * @throws java.lang.Exception If there is an error
    */
-  public static void main(final String[] args) throws Exception
-  {
-
+  public static void main(final String[] args) throws Exception {
     String app = "benchmark";
+    int size = 3;
 
     ConstellationManager demo = new ConstellationManager();
 
-    demo.InitConstellation("testconstellation");
+    demo.InitConstellation("demo", size);
     // pause to start benchmark app via CTT
     // TODO: start app
     System.in.read();
-    
-    demo.ConnectToProvider(app);    
-  }
 
-  
-  class DataReceivedAdapter extends SimpleDataReceivedListener
-  {
-
-    @Override
-    public void onDataReceived(String parameterName, Serializable data)
-    {
-      LOGGER.log(Level.INFO,
-          "\nParameter name: {0}" + "\n" + "Data content:\n{1}",
-          new Object[]{parameterName, data.toString()});
-    }
+    demo.ConnectToProviders();
   }
 }
--- mission/constellation/constellation-manager/src/main/java/esa/mo/ground/constellation/NanoSatSimulator.java
+++ mission/constellation/constellation-manager/src/main/java/esa/mo/ground/constellation/NanoSatSimulator.java
@@ -13,103 +13,168 @@
  * You on an "as is" basis and without warranties of any kind, including without
  * limitation merchantability, fitness for a particular purpose, absence of
  * defects or errors, accuracy or non-infringement of intellectual property rights.
- * 
+ *
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  * ----------------------------------------------------------------------------
- * 
+ *
  * Author: N Wiegand (https://github.com/Klabau)
  */
 package esa.mo.ground.constellation;
 
-import org.ccsds.moims.mo.mal.structures.URI;
-
+import esa.mo.mc.impl.provider.ParameterInstance;
+import esa.mo.nmf.commonmoadapter.CompleteDataReceivedListener;
+import esa.mo.nmf.commonmoadapter.SimpleDataReceivedListener;
+import esa.mo.nmf.groundmoadapter.GroundMOAdapterImpl;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.ccsds.moims.mo.common.directory.structures.ProviderSummaryList;
+import org.ccsds.moims.mo.mal.MALException;
+import org.ccsds.moims.mo.mal.MALInteractionException;
+import org.ccsds.moims.mo.mal.structures.URI;
 
 /**
  * NanoSat Segment Simulator Object
- * This object simulates the NanoSat Segment of the NMF. It is responsible for 
+ * This object simulates the NanoSat Segment of the NMF. It is responsible for
  * managing the Docker Container in which the NanoSat Segment is executed.
- * 
+ *
  */
-public class NanoSatSimulator 
-{    
-    private final String image = "nmf-rpi/nmf-supervisor"; // TODO: get image name from config
+public class NanoSatSimulator {
+  private final String image = "nmf-rpi/nmf-supervisor"; // TODO: get image name from config
+  private Logger LOGGER;
 
+  private String name;
+  private ProviderSummaryList providers;
+  private GroundMOAdapterImpl groundAdapter;
+
+  /**
+   * Initialiser Constructor.
+   * This Class manages the Docker Container which provides the NanoSat
+   * Segment for the simulated constellation.
+   *
+   * @param name Container name
+   * @param dockerClient Docker Unix Socket
+   */
+  public NanoSatSimulator(String name, Logger LOGGER) {
+    this.name = name;
+    this.LOGGER = LOGGER;
+  }
+
+  /**
+   * Create a Docker Container that will host the NanoSat Segment and start it.
+   *
+   * @throws IOException
+   */
+  public void run() throws IOException {
+    DockerApi.run(this.name, this.image);
+  }
+
+  /**
+   * Start the Docker Container that will host the NanoSat Segment
+   * @throws IOException
+   */
+  public void start() throws IOException {
+    DockerApi.start(this.name);
+  }
+
+  /**
+   * Connect to the service providers on the NanoSat Segment
+   */
+  public void connectToProviders() {
+    try {
+      this.providers =
+        GroundMOAdapterImpl.retrieveProvidersFromDirectory(
+          this.getDirectoryServiceURI()
+        );
+
+      if (!providers.isEmpty()) {
+        // Connect to provider on index 0
+        groundAdapter = new GroundMOAdapterImpl(providers.get(0));
+        groundAdapter.addDataReceivedListener(
+          new CompleteDataReceivedAdapter(this.getName())
+        );
+      } else {
+        LOGGER.log(Level.SEVERE, "The returned list of providers is empty!");
+      }
+    } catch (MALException | MALInteractionException | IOException ex) {
+      LOGGER.log(Level.SEVERE, null, ex);
+    }
+  }
+
+  /**
+   *
+   * @return Supervisor Directory Service URI
+   * @throws IOException
+   */
+  public URI getDirectoryServiceURI() throws IOException {
+    return new URI(this.getDirectoryServiceURIString());
+  }
+
+  /**
+   *
+   * @return Supervisor Directory Service URI String
+   * @throws IOException
+   */
+  public String getDirectoryServiceURIString() throws IOException {
+    // TODO: make this a bit prettier
+    return (
+      "maltcp://" +
+      this.getIPAddress() +
+      ":1024/nanosat-mo-supervisor-Directory"
+    );
+  }
+
+  /**
+   *
+   * @return Container Name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   *
+   * @return Container IP Address
+   * @throws IOException
+   */
+  public String getIPAddress() throws IOException {
+    return DockerApi.getContainerIPAddress(this.name);
+  }
+
+  private class SimpleDataReceivedAdapter extends SimpleDataReceivedListener {
     private String name;
 
-    /**
-     * Initialiser Constructor. 
-     * This Class manages the Docker Container which provides the NanoSat 
-     * Segment for the simulated constellation.
-     * 
-     * @param name Container name
-     * @param dockerClient Docker Unix Socket 
-     */
-    public NanoSatSimulator(String name)
-    {
-        this.name = name;
+    @Override
+    public void onDataReceived(String parameterName, Serializable data) {
+      LOGGER.log(
+        Level.INFO,
+        "\nNode: {0}\nParameter name: {1}\nParameter Value: {2}",
+        new Object[] { this.name, parameterName, data.toString() }
+      );
+    }
+  }
 
+  private class CompleteDataReceivedAdapter
+    extends CompleteDataReceivedListener {
+    private String name;
+
+    private CompleteDataReceivedAdapter(String name) {
+      this.name = name;
     }
 
-    /**
-     * Create a Docker Container that will host the NanoSat Segment and start it.
-     * 
-     * @throws IOException
-     */
-    public void run() throws IOException
-    {
-        DockerApi.run(this.name, this.image);
+    @Override
+    public void onDataReceived(ParameterInstance parameterInstance) {
+      LOGGER.log(
+        Level.INFO,
+        "\nNode: {0}\nParameter name: {1}\nParameter Value: {2}",
+        new Object[] {
+          this.name,
+          parameterInstance.getName(),
+          parameterInstance.getParameterValue().toString(),
+        }
+      );
     }
-
-    /**
-     * Start the Docker Container that will host the NanoSat Segment
-     * @throws IOException
-     */
-    public void start() throws IOException
-    {  
-        DockerApi.start(this.name);
-    }
-
-    /**
-     * 
-     * @return Supervisor Directory Service URI
-     * @throws IOException
-     */
-    public URI getDirectoryServiceURI() throws IOException
-    {
-        return new URI(this.getDirectoryServiceURIString());
-    }
-
-    /**
-     * 
-     * @return Supervisor Directory Service URI String
-     * @throws IOException
-     */
-    public String getDirectoryServiceURIString() throws IOException
-    {
-        // TODO: make this a bit prettier
-        return "maltcp://" 
-            + this.getIPAddress() 
-            + ":1024/nanosat-mo-supervisor-Directory";
-    }
-
-    /**
-     * 
-     * @return Container Name
-     */
-    public String getName()
-    {
-        return name;
-    }
-
-    /**
-     * 
-     * @return Container IP Address
-     * @throws IOException
-     */
-    public String getIPAddress() throws IOException
-    {
-        return DockerApi.getContainerIPAddress(this.name);
-    }
+  }
 }
