3da7058c7a09f400088de8414e194080e191c219
Cesar Coelho <cbwhittle@hotmail.com>
2022-10-28 22:09:09 +0200

    [nmf-package] Refactors the classes

--- core/nmf-package-lib/src/main/java/esa/mo/nmf/nmfpackage/Deployment.java
+++ core/nmf-package-lib/src/main/java/esa/mo/nmf/nmfpackage/Deployment.java
@@ -27,49 +27,54 @@ import java.io.File;
  * @author Cesar Coelho
  */
 public class Deployment {
-    
+
+    public static final String DIR_NMF = "nanosat-mo-framework";
+
     public static final String DIR_APPS = "apps";
     public static final String DIR_DRIVERS = "drivers";
     public static final String DIR_ETC = "etc";
     public static final String DIR_JARS_MISSION = "jars-mission";
     public static final String DIR_JARS_NMF = "jars-nmf";
-    public static final String DIR_JARS_SHARED = "jars-shared-dependencies";
+    public static final String DIR_JARS_SHARED = "jars-shared-libraries";
     public static final String DIR_JAVA = "java";
     public static final String DIR_LOGS = "logs";
     public static final String DIR_PACKAGES = "packages";
     public static final String DIR_PUBLIC = "public";
 
-    private static final String DIR_RECEIPTS = "installation_receipts";
+    private static final String DIR_INSTALLATIONS_TRACKER = "installations-tracker";
 
-    private static final String INSTALLED_RECEIPTS_FOLDER_PROPERTY = "esa.mo.nmf.nmfpackage.receipts";
+    private static final String PROPERTY_INSTALLED_RECEIPTS_FOLDER = "esa.mo.nmf.nmfpackage.receipts";
 
-    @Deprecated
-    private static final String INSTALLATION_FOLDER_PROPERTY = "esa.mo.nmf.nmfpackage.installationFolder";
-    
-    public static File getReceiptsFolder() {
+    private static String pathNMF = null;
+
+    public static File getInstallationsTrackerDir() {
         // Default location of the folder
-        File folder = new File(DIR_ETC + File.separator + DIR_RECEIPTS);
+        File folder = new File(DIR_ETC + File.separator + DIR_INSTALLATIONS_TRACKER);
 
         // Read the Property of the folder to install the packages
-        if (System.getProperty(INSTALLED_RECEIPTS_FOLDER_PROPERTY) != null) {
-            folder = new File(System.getProperty(INSTALLED_RECEIPTS_FOLDER_PROPERTY));
+        if (System.getProperty(PROPERTY_INSTALLED_RECEIPTS_FOLDER) != null) {
+            folder = new File(System.getProperty(PROPERTY_INSTALLED_RECEIPTS_FOLDER));
         }
 
         return folder;
     }
 
-    @Deprecated
-    public static File getInstallationFolder() {
-        // Default location of the folder
-        File folder = new File("");
-
-        // Read the Property of the folder to install the packages
-        if (System.getProperty(INSTALLATION_FOLDER_PROPERTY) != null) {
-            folder = new File(System.getProperty(INSTALLATION_FOLDER_PROPERTY));
+    public static File getNMFRootDir() {
+        if (pathNMF != null) {
+            return new File(pathNMF);
         }
 
+        File folder = new File((new File("")).getAbsolutePath());
+        String currentFolder = folder.getName();
+
+        // Default location of the folder
+        if (DIR_NMF.equals(currentFolder) || "supervisor".equals(currentFolder)) {
+            pathNMF = folder.getAbsolutePath();
+            return new File(pathNMF);
+        }
+
+        // We need additional logic to make sure we are in the right directory!
         return folder;
     }
 
-    
 }
