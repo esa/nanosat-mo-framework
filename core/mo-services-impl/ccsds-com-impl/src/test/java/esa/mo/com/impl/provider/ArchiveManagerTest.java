7683f46ef252fef90a032f891a198d089a1ac2a7
Lukasz.Stochlak <lukasz.stochlak@asseco.pl>
2022-11-30 12:44:18 +0100

    issue #1199: Fast objects (Archive) unit test (manual) - header update

--- core/mo-services-impl/ccsds-com-impl/src/test/java/esa/mo/com/impl/provider/ArchiveManagerTest.java
+++ core/mo-services-impl/ccsds-com-impl/src/test/java/esa/mo/com/impl/provider/ArchiveManagerTest.java
@@ -1,16 +1,23 @@
-//------------------------------------------------------------------------------
-//
-// System : reactor
-//
-// Sub-System : esa.mo.com.impl.provider
-//
-// File Name : ArchiveManagerTest.java
-//
-// Author : Lukasz.Stochlak
-//
-// Creation Date : 27.10.2022
-//
-//------------------------------------------------------------------------------
+/* ----------------------------------------------------------------------------
+ * Copyright (C) 2021      European Space Agency
+ *                         European Space Operations Centre
+ *                         Darmstadt
+ *                         Germany
+ * ----------------------------------------------------------------------------
+ * System                : ESA NanoSat MO Framework
+ * ----------------------------------------------------------------------------
+ * Licensed under European Space Agency Public License (ESA-PL) Weak Copyleft – v2.4
+ * You may not use this file except in compliance with the License.
+ *
+ * Except as expressly set forth in this License, the Software is provided to
+ * You on an "as is" basis and without warranties of any kind, including without
+ * limitation merchantability, fitness for a particular purpose, absence of
+ * defects or errors, accuracy or non-infringement of intellectual property rights.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ----------------------------------------------------------------------------
+ */
 package esa.mo.com.impl.provider;
 
 import java.util.Comparator;
