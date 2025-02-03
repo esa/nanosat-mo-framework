5082a0934c4fc0ec3b7d6e1111c2fa6e1bea8201
CesarCoelho <cbwhittle@hotmail.com>
2025-02-03 14:29:33 +0100

    Fixes pipeline tests

---
+++ core/nmf-composites/nanosat-mo-connector/src/test/java/esa/nmf/test/RegressionTest0.java
@@ -0,0 +1,15 @@
+package esa.nmf.test;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RegressionTest0 {
+
+    public static boolean debug = false;
+
+    @Test
+    public void test1() throws Throwable {
+    }
+}
