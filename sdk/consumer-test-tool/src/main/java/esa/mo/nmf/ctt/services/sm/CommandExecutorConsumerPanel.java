e0dc821c4825d0c61d66763c2b2e58ce41db7504
Cesar Coelho <cbwhittle@hotmail.com>
2026-05-14 15:43:05 +0200

    import cleanup

--- sdk/consumer-test-tool/src/main/java/esa/mo/nmf/ctt/services/sm/CommandExecutorConsumerPanel.java
+++ sdk/consumer-test-tool/src/main/java/esa/mo/nmf/ctt/services/sm/CommandExecutorConsumerPanel.java
@@ -40,7 +40,6 @@ import org.ccsds.moims.mo.mal.MALInteractionException;
 import org.ccsds.moims.mo.mal.helpertools.helpers.HelperAttributes;
 import org.ccsds.moims.mo.mal.helpertools.helpers.HelperTime;
 import org.ccsds.moims.mo.mal.structures.Element;
-import org.ccsds.moims.mo.mal.structures.Identifier;
 import org.ccsds.moims.mo.mal.structures.Subscription;
 import org.ccsds.moims.mo.mal.structures.Union;
 import org.ccsds.moims.mo.mal.transport.MALMessageHeader;
